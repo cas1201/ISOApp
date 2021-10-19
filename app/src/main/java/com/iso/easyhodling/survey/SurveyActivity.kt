04cc5f3dba03f3f5e48878dad08e0a5b9cd6716f
miguelmelero <m.mmelero1@usp.ceu.es>
2021-10-19 19:31:30 +0200

    BOTÓN DE AUTOEXCLUSIÓN Y MENSAJE TOAST PARA GUARDAR EL TEST

--- app/src/main/java/com/iso/easyhodling/survey/SurveyActivity.kt
+++ app/src/main/java/com/iso/easyhodling/survey/SurveyActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.iso.easyhodling.MainActivity
 import com.iso.easyhodling.R
 
@@ -16,5 +17,6 @@ class SurveyActivity : AppCompatActivity() {
     fun goToHome(view: View){
         startActivity(Intent(this, MainActivity::class.java))
         finish()
+        //Toast.makeText(this, R.string.testsave, Toast.LENGTH_SHORT).show()
     }
 }
