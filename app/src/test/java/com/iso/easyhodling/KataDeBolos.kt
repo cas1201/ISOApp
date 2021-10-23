7b7d74b6c91fc6d46a0e030d2a6a0ff4e5b3a4ef
cas1201 <aragones.lozano@gmail.com>
2021-10-23 12:38:04 +0200

    Test partidaDocePuntosTresTiros pasado

--- app/src/test/java/com/iso/easyhodling/KataDeBolos.kt
+++ app/src/test/java/com/iso/easyhodling/KataDeBolos.kt
@@ -10,31 +10,43 @@ class KataDeBolos {
 
     @Test
     fun partidaSinPuntos(){
-        puntuacion = partida.puntuacion(0, 0)
+        puntuacion = partida.puntuacion()
         assertEquals(0, puntuacion)
     }
 
     @Test
     fun partidaCuatroPuntos(){
-        puntuacion = partida.puntuacion(4, 0)
+        partida.turno(4, 0)
+        puntuacion = partida.puntuacion()
         assertEquals(4, puntuacion)
     }
 
     @Test
     fun partidaSietePuntos(){
-        puntuacion = partida.puntuacion(7, 0)
+        partida.turno(7, 0)
+        puntuacion = partida.puntuacion()
         assertEquals(7, puntuacion)
     }
 
     @Test
     fun partidaCuatroPuntosDosTiros(){
-        puntuacion = partida.puntuacion(2, 2)
+        partida.turno(2, 2)
+        puntuacion = partida.puntuacion()
         assertEquals(4, puntuacion)
     }
 
     @Test
     fun partidaSietePuntosDosTiros(){
-        puntuacion = partida.puntuacion(2, 5)
+        partida.turno(2, 5)
+        puntuacion = partida.puntuacion()
         assertEquals(7, puntuacion)
+    }
+
+    @Test
+    fun partidaDocePuntosTresTiros(){
+        partida.turno(5, 4)
+        partida.turno(3, 0)
+        puntuacion = partida.puntuacion()
+        assertEquals(12, puntuacion)
     }
 }
--- app/src/test/java/com/iso/easyhodling/Partida.kt
+++ app/src/test/java/com/iso/easyhodling/Partida.kt
@@ -1,10 +1,26 @@
 package com.iso.easyhodling
 
+import java.lang.Integer.sum
+
 class Partida {
     private var puntuacion = 0
+    private var turno = mutableListOf<Int>()
+    private var partida = mutableListOf<MutableList<Int>>()
+    private var ultimoTurno = 0
 
-    fun puntuacion(bolosTiradosPrimeraTirada: Int, bolosTiradosSegundaTirada: Int): Int {
-        puntuacion += bolosTiradosPrimeraTirada + bolosTiradosSegundaTirada
+    fun puntuacion(): Int{
+        for (i in partida.indices)
+            puntuacion += partida[i].sum()
         return puntuacion
+    }
+
+    fun turno(tirada1: Int, tirada2: Int){
+        turno = mutableListOf(tirada1, tirada2)
+        partida(turno)
+    }
+
+    private fun partida(turno: MutableList<Int>) {
+        partida.add(ultimoTurno, turno)
+        ultimoTurno++
     }
 }
