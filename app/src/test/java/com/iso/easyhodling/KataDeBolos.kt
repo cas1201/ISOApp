a2ba37de4d6801d4c8b212b645df1444db32d31b
cas1201 <aragones.lozano@gmail.com>
2021-10-23 14:38:41 +0200

    Test partidaUnSemiplenoYPuntuarPrimeraTiradaSegundoTurno pasado

--- app/src/test/java/com/iso/easyhodling/KataDeBolos.kt
+++ app/src/test/java/com/iso/easyhodling/KataDeBolos.kt
@@ -10,51 +10,52 @@ class KataDeBolos {
 
     @Test
     fun partidaSinPuntos(){
-        puntuacion = partida.puntuacion()
+        puntuacion = partida.turno(0, 0)
         assertEquals(0, puntuacion)
     }
 
     @Test
     fun partidaCuatroPuntos(){
-        partida.turno(4, 0)
-        puntuacion = partida.puntuacion()
+        puntuacion = partida.turno(4, 0)
         assertEquals(4, puntuacion)
     }
 
     @Test
     fun partidaSietePuntos(){
-        partida.turno(7, 0)
-        puntuacion = partida.puntuacion()
+        puntuacion = partida.turno(7, 0)
         assertEquals(7, puntuacion)
     }
 
     @Test
     fun partidaCuatroPuntosDosTiros(){
-        partida.turno(2, 2)
-        puntuacion = partida.puntuacion()
+        puntuacion = partida.turno(2, 2)
         assertEquals(4, puntuacion)
     }
 
     @Test
     fun partidaSietePuntosDosTiros(){
-        partida.turno(2, 5)
-        puntuacion = partida.puntuacion()
+        puntuacion = partida.turno(2, 5)
         assertEquals(7, puntuacion)
     }
 
     @Test
     fun partidaDocePuntosTresTiros(){
         partida.turno(5, 4)
-        partida.turno(3, 0)
-        puntuacion = partida.puntuacion()
+        puntuacion = partida.turno(3, 0)
         assertEquals(12, puntuacion)
     }
 
     @Test
     fun partidaDocePuntosCuatroTiros(){
         partida.turno(4, 4)
-        partida.turno(3, 1)
-        puntuacion = partida.puntuacion()
+        puntuacion = partida.turno(3, 1)
         assertEquals(12, puntuacion)
+    }
+
+    @Test
+    fun partidaUnSemiplenoYPuntuarPrimeraTiradaSegundoTurno(){
+        puntuacion =
+            partida.turno(5, 5) + partida.turno(2, 0)
+        assertEquals(14, puntuacion)
     }
 }
--- app/src/test/java/com/iso/easyhodling/Partida.kt
+++ app/src/test/java/com/iso/easyhodling/Partida.kt
@@ -8,15 +8,22 @@ class Partida {
     private var partida = mutableListOf<MutableList<Int>>()
     private var ultimoTurno = 0
 
-    fun puntuacion(): Int{
-        for (i in partida.indices)
-            puntuacion += partida[i].sum()
+    fun puntuacion(): Int {
+        if (ultimoTurno != 1) {
+            if (partida[ultimoTurno - 2].sum() == 10)
+                puntuacion = partida[ultimoTurno - 1].sum() + partida[ultimoTurno - 1][0]
+            else
+                puntuacion += partida[ultimoTurno - 1].sum()
+        } else
+            puntuacion += partida[ultimoTurno - 1].sum()
+
         return puntuacion
     }
 
-    fun turno(tirada1: Int, tirada2: Int){
+    fun turno(tirada1: Int, tirada2: Int): Int {
         turno = mutableListOf(tirada1, tirada2)
         partida(turno)
+        return puntuacion()
     }
 
     private fun partida(turno: MutableList<Int>) {
