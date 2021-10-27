e513f95b27c0614d7ac0f703f27efa32368c75e2
cas1201 <aragones.lozano@gmail.com>
2021-10-27 14:13:10 +0200

    Kata con tests de plenos, semiplenos y partidas random pasados

--- app/src/test/java/com/iso/easyhodling/KataDeBolos.kt
+++ app/src/test/java/com/iso/easyhodling/KataDeBolos.kt
@@ -1,82 +1,198 @@
 package com.iso.easyhodling
 
+import com.iso.easyhodling.Partida.Ronda.ronda
 import org.junit.Assert.*
 import org.junit.Test
 
 class KataDeBolos {
 
     private val partida = Partida()
-    private var puntuacion = 0
+
+    fun añadirRestoRondas(){
+        val rondasTotales = 10
+        while (ronda - 1 != rondasTotales){
+            partida.turno(0, 0)
+        }
+    }
 
     @Test
     fun partidaSinPuntos(){
-        puntuacion = partida.turno(0, 0)
-        assertEquals(0, puntuacion)
+        partida.turno(0, 0)
+        añadirRestoRondas()
+        assertEquals(0, partida.puntuacion())
     }
 
     @Test
     fun partidaCuatroPuntos(){
-        puntuacion = partida.turno(4, 0)
-        assertEquals(4, puntuacion)
+        partida.turno(4, 0)
+        añadirRestoRondas()
+        assertEquals(4, partida.puntuacion())
     }
 
     @Test
     fun partidaSietePuntos(){
-        puntuacion = partida.turno(7, 0)
-        assertEquals(7, puntuacion)
+        partida.turno(7, 0)
+        añadirRestoRondas()
+        assertEquals(7, partida.puntuacion())
     }
 
     @Test
     fun partidaCuatroPuntosDosTiros(){
-        puntuacion = partida.turno(2, 2)
-        assertEquals(4, puntuacion)
+        partida.turno(2, 2)
+        añadirRestoRondas()
+        assertEquals(4, partida.puntuacion())
     }
 
     @Test
     fun partidaSietePuntosDosTiros(){
-        puntuacion = partida.turno(2, 5)
-        assertEquals(7, puntuacion)
+        partida.turno(2, 5)
+        añadirRestoRondas()
+        assertEquals(7, partida.puntuacion())
     }
 
     @Test
     fun partidaDocePuntosTresTiros(){
         partida.turno(5, 4)
-        puntuacion = partida.turno(3, 0)
-        assertEquals(12, puntuacion)
+        partida.turno(3, 0)
+        añadirRestoRondas()
+        assertEquals(12, partida.puntuacion())
     }
 
     @Test
     fun partidaDocePuntosCuatroTiros(){
         partida.turno(4, 4)
-        puntuacion = partida.turno(3, 1)
-        assertEquals(12, puntuacion)
+        partida.turno(3, 1)
+        añadirRestoRondas()
+        assertEquals(12, partida.puntuacion())
     }
 
     @Test
     fun partidaUnSemiplenoYPuntuarPrimeraTiradaSegundoTurno(){
-        puntuacion =
-            partida.turno(5, 5) + partida.turno(2, 0)
-        assertEquals(14, puntuacion)
+        partida.turno(5, 5)
+        partida.turno(2, 0)
+        añadirRestoRondas()
+        assertEquals(14, partida.puntuacion())
     }
 
     @Test
     fun partidaUnSemiplenoYPuntuarAmbasTiradasSegundoTurno(){
-        puntuacion =
-            partida.turno(5, 5) + partida.turno(2, 3)
-        assertEquals(17, puntuacion)
+        partida.turno(5, 5)
+        partida.turno(2, 3)
+        añadirRestoRondas()
+        assertEquals(17, partida.puntuacion())
     }
 
     @Test
     fun partidaUnPlenoYPuntuarPrimeraTiradaSegundoTurno(){
-        puntuacion =
-            partida.turno(10, 0) + partida.turno(2, 0)
-        assertEquals(14, puntuacion)
+        partida.turno(10, 0)
+        partida.turno(2, 0)
+        añadirRestoRondas()
+        assertEquals(14, partida.puntuacion())
     }
 
     @Test
     fun partidaUnPlenoYPuntuarAmbasTiradasSegundoTurno(){
-        puntuacion =
-            partida.turno(10, 0) + partida.turno(2, 3)
-        assertEquals(20, puntuacion)
+        partida.turno(10, 0)
+        partida.turno(2, 3)
+        añadirRestoRondas()
+        assertEquals(20, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaDosSemiplenos(){
+        partida.turno(6, 4)
+        partida.turno(2, 8)
+        añadirRestoRondas()
+        assertEquals(22, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaDosSemiplenosYPuntuarPrimeraTiradaSiguienteTurno(){
+        partida.turno(6, 4)
+        partida.turno(2, 8)
+        partida.turno(3, 0)
+        añadirRestoRondas()
+        assertEquals(28, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaDosSemiplenosYPuntuarAmbasTiradasSiguienteTurno(){
+        partida.turno(6, 4)
+        partida.turno(2, 8)
+        partida.turno(3, 4)
+        añadirRestoRondas()
+        assertEquals(32, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaDosPlenos(){
+        partida.turno(10, 0)
+        partida.turno(10, 0)
+        añadirRestoRondas()
+        assertEquals(30, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaTresPlenos(){
+        partida.turno(10, 0)
+        partida.turno(10, 0)
+        partida.turno(10, 0)
+        añadirRestoRondas()
+        assertEquals(60, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaTresPlenosYPuntuarAmbasTiradasSiguienteTurno(){
+        partida.turno(10, 0)
+        partida.turno(10, 0)
+        partida.turno(10, 0)
+        partida.turno(2, 1)
+        añadirRestoRondas()
+        assertEquals(68, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaTresPlenosYPuntuarAmbasTiradasSiguienteTurno2(){
+        partida.turno(10, 0)
+        partida.turno(10, 0)
+        partida.turno(10, 0)
+        partida.turno(1, 1)
+        añadirRestoRondas()
+        assertEquals(65, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaRandom1(){
+        partida.turno(5, 3)
+        partida.turno(6, 3)
+        partida.turno(3, 7)
+        partida.turno(2, 4)
+        añadirRestoRondas()
+        assertEquals(35, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaRandom2(){
+        partida.turno(5, 3)
+        partida.turno(10, 0)
+        partida.turno(3, 7)
+        partida.turno(2, 4)
+        añadirRestoRondas()
+        assertEquals(46, partida.puntuacion())
+    }
+
+    @Test
+    fun partidaRandom3(){
+        partida.turno(0, 0)
+        partida.turno(0, 0)
+        partida.turno(0, 0)
+        partida.turno(0, 0)
+        partida.turno(0, 0)
+        partida.turno(0, 0)
+        partida.turno(0, 0)
+        partida.turno(0, 0)
+        partida.turno(0, 0)
+        partida.turno(10, 0)
+        assertEquals(10, partida.puntuacion())
     }
 }
--- app/src/test/java/com/iso/easyhodling/Partida.kt
+++ app/src/test/java/com/iso/easyhodling/Partida.kt
@@ -1,38 +1,49 @@
 package com.iso.easyhodling
 
-import java.lang.Integer.sum
-
 class Partida {
+    companion object Ronda {
+        var ronda = 1
+    }
     private var puntuacion = 0
+    private var ultimoTurno = 0
     private var turno = mutableListOf<Int>()
     private var partida = mutableListOf<MutableList<Int>>()
-    private var ultimoTurno = 0
 
     fun puntuacion(): Int {
-        if (ultimoTurno != 1) {
-            if (partida[ultimoTurno - 2].sum() == 10) {
-                if (partida[ultimoTurno - 2][0] == 10) {
-                    puntuacion =
-                        partida[ultimoTurno - 1].sum() + partida[ultimoTurno - 1].sum()
-                } else
-                    puntuacion =
-                        partida[ultimoTurno - 1].sum() + partida[ultimoTurno - 1][0]
-            } else
-                puntuacion += partida[ultimoTurno - 1].sum()
-        } else
-            puntuacion += partida[ultimoTurno - 1].sum()
-
+        while (ultimoTurno != partida.size) {
+            if (ultimoTurno != 0) {
+                if (partida[ultimoTurno - 1].sum() == 10) {
+                    if (partida[ultimoTurno - 1][0] == 10) {
+                        if (partida[ultimoTurno][0] == 10) {
+                            puntuacion += partida[ultimoTurno].sum() + partida[ultimoTurno][0] + partida[ultimoTurno + 1][0]
+                            ultimoTurno++
+                        } else {
+                            puntuacion += partida[ultimoTurno].sum() + partida[ultimoTurno][0] + partida[ultimoTurno][1]
+                            ultimoTurno++
+                        }
+                    } else {
+                        puntuacion += partida[ultimoTurno].sum() + partida[ultimoTurno][0]
+                        ultimoTurno++
+                    }
+                } else {
+                    puntuacion += partida[ultimoTurno].sum()
+                    ultimoTurno++
+                }
+            } else {
+                puntuacion += partida[ultimoTurno].sum()
+                ultimoTurno++
+            }
+        }
         return puntuacion
     }
 
-    fun turno(tirada1: Int, tirada2: Int): Int {
+    fun turno(tirada1: Int, tirada2: Int) {
         turno = mutableListOf(tirada1, tirada2)
         partida(turno)
-        return puntuacion()
     }
 
     private fun partida(turno: MutableList<Int>) {
-        partida.add(ultimoTurno, turno)
-        ultimoTurno++
+        partida.add(ronda - 1, turno)
+        ronda++
     }
 }
