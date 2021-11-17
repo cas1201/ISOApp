ef79deb4242fe1b8eca2fa5a67682a19211f55e3
mikim <m.melero1@usp.ceu.es>
2021-11-17 18:18:00 +0100

    falta terminar la send email e incluir el boton

--- app/src/main/java/com/iso/easyhodling/login/PasswordForgottenActivity.kt
+++ app/src/main/java/com/iso/easyhodling/login/PasswordForgottenActivity.kt
@@ -6,28 +6,35 @@ import android.os.Bundle
 import android.util.Patterns
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.tasks.Task
 import com.iso.easyhodling.R
-import com.iso.easyhodling.databinding.ActivityCreateAccountBinding
 import com.iso.easyhodling.databinding.ActivityPasswordForgottenBinding
-import java.util.regex.Pattern
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
+import com.google.android.gms.tasks.OnCompleteListener
 
 class PasswordForgottenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPasswordForgottenBinding
     private lateinit var passwordForgottenViewModel: PasswordForgottenViewModel
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPasswordForgottenBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_password_forgotten)
         passwordForgottenViewModel= ViewModelProvider(this).get(PasswordForgottenViewModel::class.java)
+        // INCLUIR EL BOTON
+    // editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress)
+
     }
     fun onClick(view: View){
-
+    validate()
     }
 
     //validar si el email es válido
-    fun validate(view: View){
+    fun validate() {
         val email = binding.editTextTextEmailAddress.text.toString()
 
         if ((email.isEmpty()) || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
@@ -36,19 +43,55 @@ class PasswordForgottenActivity : AppCompatActivity() {
         //sendEmail(email)
 
     }
-    //para ir al login
-    fun gotoLogin(view:View){
-        startActivity(Intent(this, LoginActivity::class.java))
-        finish()
+    //Flecha para que se vaya al Login activity en caso que le de sin querer
+    override fun onBackPressed() {
+        super.onBackPressed()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
     }
-    //https://firebase.google.com/docs/android/setup?hl=es PARA TERMINAR LO DE MANDAR EMAIL
+/* clase que modificar
+    fun sendEmail(view: View) {
+        //FirebaseAuth auth = FirebaseAuth.getInstance()
+        val email = binding.editTextTextEmailAddress.text.toString()
 
-    /*fun sendEmail(view: View){
+        val addOnCompleteListener = auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener(com.google.android.gms.tasks.OnCompleteListener {
+                if (task.isSuccessful()) {
+                    Toast.makeText(this,"Correo enviado!", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }else {
+                    Toast.makeText(this,"Correo invalido!", Toast.LENGTH_SHORT).show())
+                }
+    }*/
+
+        /* para ir al login, TAMBIÉN VALDRÍA ESTA OPCIÓN
+         fun gotoLogin(view:View){
+             startActivity(Intent(this, LoginActivity::class.java))
+             finish()
+         }
+         //https://firebase.google.com/docs/android/setup?hl=es PARA TERMINAR LO DE MANDAR EMAIL
+
+     */
+
+
+//OTRA FORMA DE HACERLO
+/* fun sendEmail(view: View){
         FirebaseAuth auth = FirebaseAuth.getInstance()
         val email = binding.editTextTextEmailAddress.text.toString()
 
         auth.sendPasswordResetEmail()
-    } */
+    }
+   // función para la verificación
+   private fun sendEmailVerification() {
+
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // manda la verificación del email
+            }
+*/
+    }
 
     //funcion de volver en caso de haber pinchado en recuperar contraseña sin querer en el login, hacer una función para el botón de back
 
